012e1dc41e6ca05a6e2c12233976fc7e33468e8c
zwonH <a.circle.one@gmail.com>
2020-04-05 18:22:01 +0900

    6: todos 필터 함수 구현

--- class5_ArrayObject/ass3-1-3_reduce.js
+++ class5_ArrayObject/ass3-1-3_reduce.js
@@ -1,2 +1,8 @@
 // reduce 만들기
 // Array 의 reduce 메서드처럼 동작하는 myReduce 메서드 만들기
+
+const myReduce = (arr, callback, initialValue) => {
+    //여기에 구현
+}
+
+const result = myReduce(arr, (next,prev) => {...}, []);
---
+++ class6_Async/todos.js
@@ -0,0 +1,43 @@
+const todos = [
+  {
+    name: '자바스크립트 공부하기',
+    tags: ['programming', 'javascript'],
+    status: 'todo',
+    id: 1,
+  },
+  {
+    name: '리덕스 공부하기',
+    tags: ['programming', 'redux'],
+    status: 'doing',
+    id: 2,
+  },
+  {
+    name: '그림 그리기',
+    tags: ['picture', 'favorite'],
+    status: 'done',
+    id: 3,
+  },
+  {
+    name: '유튜브 업로드',
+    tags: ['youtube', 'favorite'],
+    status: 'todo',
+    id: 3,
+  },
+];
+
+const show = obj => {
+  switch (obj) {
+    case 'all':
+      let todo = todos.filter(el => el.status === 'todo').length;
+      let doing = todos.filter(el => el.status === 'doing').length;
+      let done = todos.filter(el => el.status === 'done').length;
+      console.log(`현재상태: todo: ${todo}개, doing: ${doing}개, done: ${done}개`);
+      break;
+    case 'todo':
+      let todolist = todos.filter(el => el.status === 'todo');
+      console.log(`총 ${todolist.length}건: ${todolist.map(el => el.name)}`);
+  }
+};
+
+show('all');
+show('todo');
